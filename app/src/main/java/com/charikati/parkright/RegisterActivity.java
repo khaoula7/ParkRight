4211137ef063a3763b8cace848790e130a296307
Khaoula <tej,khaoula@gmail.com>
2019-08-16 20:55:57 +0400

    Summary activity layout

--- app/src/main/java/com/charikati/parkright/RegisterActivity.java
+++ app/src/main/java/com/charikati/parkright/RegisterActivity.java
@@ -1,20 +1,14 @@
 package com.charikati.parkright;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -54,7 +48,7 @@ public class RegisterActivity extends BaseActivity {
         mEmailField = findViewById(R.id.email_edit_text);
         mPasswordField = findViewById(R.id.password_edit_text);
         mTermsCheck = findViewById(R.id.terms_chk_box);
-        mRegisterBtn = findViewById(R.id.register_btn);
+        mRegisterBtn = findViewById(R.id.send_btn);
 
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/charikati/parkright/SummaryActivity.java
+++ app/src/main/java/com/charikati/parkright/SummaryActivity.java
@@ -5,36 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+
 public class SummaryActivity extends AppCompatActivity {
     private Button mLogOutButton;
     private FirebaseAuth mAuth;
+    private Spinner mSpinner;
+    private ArrayList mViolationData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+        mSpinner = findViewById(R.id.spinner);
+        String[] violations = getResources().getStringArray(R.array.violation_types);
+        ArrayAdapter<String> violationAdapter=new ArrayAdapter<String>(this,R.layout.spinner_item, violations);
+        mSpinner.setAdapter(violationAdapter);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
 
-        /*mLogOutButton = findViewById(R.id.logout_btn);
-        mLogOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Logout from Firebase
-                mAuth.signOut();
-                //Logout from Facebook
-                LoginManager.getInstance().logOut();
-                updateUI();
-            }
-        });*/
+
+
+
     }
 
     @Override
@@ -45,10 +45,10 @@ public class SummaryActivity extends AppCompatActivity {
         //GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         //if(account != null) {
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser == null){
-            updateUI();
-        }
+        //FirebaseUser currentUser = mAuth.getCurrentUser();
+        //if(currentUser == null){
+          //  updateUI();
+        //}
 
 
     }
